a3dbf6532fb3538192ad64abf77373bfff2d04bd
jon-cron <jonathanacm95@gmail.com>
2022-12-07 14:23:13 -0700

    connected front to back

--- src/env.js
+++ src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'mickshanny.us.auth0.com'
-export const clientId = 'luwNBP378EfKtXGGgxJj9T2kNZ1A562I'
-export const audience = 'http://Sandbox.com'
+export const domain = 'dev-esg3qw0wv4oc3r11.us.auth0.com'
+export const clientId = 'hulGXtuEYnFl2eBkOovdhEdr7rwSZ9J1'
+export const audience = 'https://classroom.com'
